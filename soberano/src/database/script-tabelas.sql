286ea1939fefee44c8501c8e2656612fbd49275c
Beatriz Arruda <160678773+bmarruda@users.noreply.github.com>
2024-05-21 15:13:25 -0300

    Subindo Site

---
+++ soberano/src/database/script-tabelas.sql
@@ -0,0 +1,60 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql server
+*/
+
+CREATE DATABASE soberano;
+
+USE soberano;
+
+CREATE TABLE jogador (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nomeJogador VARCHAR(50),
+	camisa CHAR(2),
+	Posicao varchar(20)
+);
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50),
+	fk_jogador INT,
+	FOREIGN KEY (fk_jogador) REFERENCES jogador(id)
+);
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
+
+create table aquario (
+/* em nossa regra de negócio, um aquario tem apenas um sensor */
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	descricao VARCHAR(300),
+	fk_jogador INT,
+	FOREIGN KEY (fk_jogador) REFERENCES jogador(id)
+);
+
+/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+
+create table medida (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	dht11_umidade DECIMAL,
+	dht11_temperatura DECIMAL,
+	luminosidade DECIMAL,
+	lm35_temperatura DECIMAL,
+	chave TINYINT,
+	momento DATETIME,
+	fk_aquario INT,
+	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+);
+
+insert into jogador  values ('Calleri', '09',);
+insert into aquario (descricao, fk_jogador) values ('Aquário de Estrela-do-mar', 1);
