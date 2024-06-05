2c0103c7456d0fbd3de016df58c01b55362b36ab
bmarruda <beatriz.arruda@sptech.school>
2024-06-05 03:43:39 -0300

    grafico

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -1,46 +1,52 @@
-var database = require("../database/config");
+// var database = require("../database/config");
 
 function buscarUltimasMedidas(idAquario, limite_linhas) {
-    var instrucaoSql = `SELECT
-        dht11_temperatura as temperatura,
+    var instrucaoSql = `SELECT 
+        dht11_temperatura as temperatura, 
         dht11_umidade as umidade,
         momento,
         DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-        FROM medida
-        WHERE fk_aquario = ${idAquario}
-        ORDER BY id DESC LIMIT ${limite_linhas}`;
+    FROM medida
+    WHERE fk_aquario = ${idAquario}
+    ORDER BY id DESC LIMIT ${limite_linhas}`;
 
-    console.log("Executando a instrução SQL para buscar as últimas medidas: \n" + instrucaoSql);
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 function buscarMedidasEmTempoReal(idAquario) {
-    var instrucaoSql = `SELECT
-        dht11_temperatura as temperatura,
+    var instrucaoSql = `SELECT 
+        dht11_temperatura as temperatura, 
         dht11_umidade as umidade,
-        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico,
-        fk_aquario
-        FROM medida WHERE fk_aquario = ${idAquario}
-        ORDER BY id DESC LIMIT 1`;
+        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
+        fk_aquario 
+    FROM medida 
+    WHERE fk_aquario = ${idAquario} 
+    ORDER BY id DESC LIMIT 1`;
 
-    console.log("Executando a instrução SQL para buscar medidas em tempo real: \n" + instrucaoSql);
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function obterDadosJogadores() {
-    var instrucaoSql = `SELECT
-        jogador.nomeJogador,
-        COUNT(usuario.id) as quantidade
-        FROM jogador
-        LEFT JOIN usuario ON jogador.idJogador = usuario.fkjogador
-        GROUP BY jogador.idJogador`;
+function enviarJogador(id, fkJogador) {
+    var instrucaoSql = `INSERT INTO usuario (id, fkjogador) VALUES (${id}, ${fkJogador})`;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
 
-    console.log("Executando a instrução SQL para obter os dados dos jogadores: \n" + instrucaoSql);
+function buscarEscolhasDosJogadores() {
+    var instrucaoSql = `SELECT jogador.nomeJogador, COUNT(usuario.id) AS quantidade
+    FROM usuario
+    JOIN jogador ON usuario.fkjogador = jogador.idJogador
+    GROUP BY jogador.nomeJogador`;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 module.exports = {
     buscarUltimasMedidas,
     buscarMedidasEmTempoReal,
-    obterDadosJogadores
+    enviarJogador,
+    buscarEscolhasDosJogadores
 };
--- src/routes/medidas.js
+++ src/routes/medidas.js
@@ -1,21 +1,44 @@
+// var express = require("express");
+// var router = express.Router();
+
+
+// var medidaController = require("../controllers/medidaController");
+
+
+// router.get("/ultimas", function (req, res) {
+//     medidaController.buscarUltimasMedidas(req, res);
+// });
+
+
+// router.get("/tempo-real", function (req, res) {
+//     medidaController.buscarMedidasEmTempoReal(req, res);
+// });
+
+// router.get("/dados-jogadores", function(req, res) {
+//     medidaController.obterDadosJogadores(req, res);
+// });
+
+// module.exports = router;
+
 var express = require("express");
 var router = express.Router();
-
-
 var medidaController = require("../controllers/medidaController");
 
-
-router.get("/ultimas", function (req, res) {
+router.get("/ultimas/", function (req, res) {
     medidaController.buscarUltimasMedidas(req, res);
 });
 
-
-router.get("/tempo-real", function (req, res) {
+router.get("/tempo-real/", function (req, res) {
     medidaController.buscarMedidasEmTempoReal(req, res);
 });
 
-router.get("/dados-jogadores", function(req, res) {
-    medidaController.obterDadosJogadores(req, res);
+router.post("/jogador/", function (req, res) {
+    medidaController.enviarJogador(req, res);
+});
+
+// Nova rota para buscar as escolhas dos jogadores
+router.get("/escolhas-jogadores/", function (req, res) {
+    medidaController.buscarEscolhasDosJogadores(req, res);
 });
 
 module.exports = router;
