6e27d0a80b5e908c99c009cddea72eb4a70a0e4b
bmarruda <beatriz.arruda@sptech.school>
2024-06-06 11:36:37 -0300

    ajustes finais

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,11 +1,3 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
-/*
-comandos para mysql server
-*/
-
 CREATE DATABASE soberano;
 
 USE soberano;
@@ -58,3 +50,8 @@ insert into jogador (nomeJogador, posicao)values
 ('William Gomes', 'Atacante');
 
 select * from usuario;
+
+SELECT jogador.nomeJogador, COUNT(usuario.id) AS quantidade
+    FROM usuario
+    JOIN jogador ON usuario.fkjogador = jogador.idJogador
+    GROUP BY jogador.nomeJogador;
--- src/routes/medidas.js
+++ src/routes/medidas.js
@@ -1,25 +1,3 @@
-// var express = require("express");
-// var router = express.Router();
-
-
-// var medidaController = require("../controllers/medidaController");
-
-
-// router.get("/ultimas", function (req, res) {
-//     medidaController.buscarUltimasMedidas(req, res);
-// });
-
-
-// router.get("/tempo-real", function (req, res) {
-//     medidaController.buscarMedidasEmTempoReal(req, res);
-// });
-
-// router.get("/dados-jogadores", function(req, res) {
-//     medidaController.obterDadosJogadores(req, res);
-// });
-
-// module.exports = router;
-
 var express = require("express");
 var router = express.Router();
 var medidaController = require("../controllers/medidaController");
