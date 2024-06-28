fa9f5a3257444675b98d33a6f61630560ab199b2
bmarruda <beatriz.arruda@sptech.school>
2024-06-27 22:28:25 -0300

    registro na tabela e  consultar o ranking

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -35,9 +35,29 @@ function buscarEscolhasDosJogadores() {
     return database.executar(instrucaoSql);
 }
 
+// Essa função insere um novo registro na tabela respostas (27/06)
+function enviarQuiz(idUsuario, acertos) {
+    var instrucaoSql = `INSERT INTO respostas (fkusuario,acertos) VALUES
+    (${idUsuario},${acertos})`
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+// Função para consultar o ranking via novo select adcionado ao banco de dados (27/06)
+function buscarPosition() {
+
+    var instrucaoSql = ` SELECT u.nome, r.acertos
+FROM respostas r
+JOIN usuario u ON r.fkusuario = u.idusuario
+ORDER BY r.acertos DESC
+LIMIT 3;`
+    console.log("Executando a dashboard de ranking: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
 module.exports = {
     buscarUltimasMedidas,
     buscarMedidasEmTempoReal,
     enviarJogador,
-    buscarEscolhasDosJogadores
+    buscarEscolhasDosJogadores,
+    enviarQuiz,
+    buscarPosition
 };
